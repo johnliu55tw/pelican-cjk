52ed1708f484039febe1ebe996ac95f6ff9679c5
John Liu <johnliu55tw@gmail.com>
2019-05-14 00:52:33 +0800

    Added spaces between CJK and ANS characters.

--- pelican_cjk_correct_spaces.py
+++ pelican_cjk_correct_spaces.py
@@ -5,6 +5,18 @@ import logging
 from pelican import signals
 
 
+def ranges_as_regex(ranges):
+    pattern_text = '['
+    for start, end in ranges:
+        if end == '':
+            pattern_text += start
+        else:
+            pattern_text += '{}-{}'.format(start, end)
+    pattern_text += ']'
+
+    return pattern_text
+
+
 CJK_RANGES = (
     # References: https://en.wikipedia.org/wiki/CJK_Unified_Ideographs
     (u'\u4E00', u'\u9FFF'),             # CJK Unified Ideographs
@@ -17,10 +29,8 @@ CJK_RANGES = (
     (u'\uF900', u'\uFAFF'),             # CJK Compatibility Ideographs
     (u'\U0002F800', u'\U0002FA1F'),     # CJK Compatibility Ideographs Supplement
     # References: https://stackoverflow.com/a/30200250
-    (u'\u3000', u'\u303F'),             # Japanese-style punctuation
     (u'\u3040', u'\u309F'),             # Hiragana
     (u'\u30A0', u'\u30FF'),             # Katakana
-    (u'\uFF00', u'\uFFEF'),             # Full-width roman characters and half-width katakana
     (u'\u4E00', u'\u9FAF'),             # CJK unified Ideographs
     # References: https://en.wikipedia.org/wiki/Korean_language_and_computers#Hangul_in_Unicode
     (u'\uAC00', u'\uD7A3'),             # Hangul Syllables
@@ -30,28 +40,50 @@ CJK_RANGES = (
     (u'\uD7B0', u'\uD7FF'),             # Hangul Jamo Ext. B
 )
 
+# Alphabets, Numbers and Symbols
+ANS_RANGES = (
+    ('a', 'z'),
+    ('A', 'Z'),
+    ('0', '9'),
+    ("'", ''),                          # Single quote
+    (r'`~@#%"/_=,:\!\$\^\*\(\)\-\+\[\]\{\}\\\|\.\?', ''),  # Characters <>&;' are removed
+    (u'\u00A1', u'\u00FF'),
+    (u'\u0370', u'\u03FF'),             # Greek and Coptic
+)
 
-def cjk_regex_range():
-    pattern_text = '['
-    for start, end in CJK_RANGES:
-        pattern_text += '{}-{}'.format(start, end)
-    pattern_text += ']'
 
-    return pattern_text
+newline_pattern = re.compile(r'{}({})'.format(os.linesep, ranges_as_regex(CJK_RANGES)))
 
+cjk_ans_pattern = re.compile(r'({})({})'.format(
+    ranges_as_regex(CJK_RANGES), ranges_as_regex(ANS_RANGES)))
 
-newline_pattern = re.compile(r'{}({})'.format(os.linesep, cjk_regex_range()))
+ans_cjk_pattern = re.compile(r'({})({})'.format(
+    ranges_as_regex(ANS_RANGES), ranges_as_regex(CJK_RANGES)))
 
 
 def remove_newline_spaces(text):
     return newline_pattern.sub(r'\1', text)
 
 
+def add_space(text):
+    ret = text
+
+    ret = cjk_ans_pattern.sub(r'\1 \2', ret)
+    ret = ans_cjk_pattern.sub(r'\1 \2', ret)
+
+    return ret
+
+
 def main(content):
     if content._content is None:
         return
 
-    content._content = remove_newline_spaces(content._content)
+    ret = content._content
+
+    ret = remove_newline_spaces(ret)
+    ret = add_space(ret)
+
+    content._content = ret
 
 
 def register():
--- tests/test_remove_newline_spaces.py
+++ tests/test_remove_newline_spaces.py
@@ -19,28 +19,46 @@ CJK_WITH_NEWLINES = read_test_data('cjk_with_newlines.html')
 ENG_WITH_NEWLINES = read_test_data('eng_with_newlines.html')
 
 
-class RangesAsReTestCase(unittest.TestCase):
+class RangesAsRegexTestCase(unittest.TestCase):
 
-    def test_search_found(self):
-        p = re.compile(plugin.cjk_regex_range())
+    def test_ranges_with_start_and_end(self):
+        data = (
+            ('a', 'z'),
+            ('\u00A0', '\u00AF'),
+        )
 
-        result = p.search('abcd\U0002A701efgh')
+        re_range = plugin.ranges_as_regex(data)
 
-        self.assertEqual(result.start(), 4)
-        self.assertEqual(result.end(), 5)
+        self.assertEqual(re_range, '[a-z\u00A0-\u00AF]')
 
-    def test_search_not_found(self):
-        p = re.compile(plugin.cjk_regex_range())
+    def test_ranges_without_end(self):
+        data = (
+            ('A', ''),
+            ('\u00A0\u00A1', ''),
+            ('!@#$%', ''),
+        )
 
-        result = p.search('abcd\uA000efgh')
+        re_range = plugin.ranges_as_regex(data)
 
-        self.assertIsNone(result)
+        self.assertEqual(re_range, '[A\u00A0\u00A1!@#$%]')
+
+    def test_ranges_mixed(self):
+        data = (
+            ('A', 'Z'),
+            ('\u00A0', ''),
+            ('!@#', ''),
+            ('a', 'z'),
+        )
+
+        re_range = plugin.ranges_as_regex(data)
+
+        self.assertEqual(re_range, '[A-Z\u00A0!@#a-z]')
 
 
 class CjkRangeTestCase(unittest.TestCase):
 
     def is_cjk(self, c):
-        return re.match(plugin.cjk_regex_range(), c) is not None
+        return re.match(plugin.ranges_as_regex(plugin.CJK_RANGES), c) is not None
 
     def test_pattern_matched(self):
         self.assertTrue(self.is_cjk('我'))
@@ -76,3 +94,48 @@ class RemoveNewlineTestCase(unittest.TestCase):
         result = plugin.remove_newline_spaces(ENG_WITH_NEWLINES)
 
         self.assertIn('First line,\nsecond line', result)
+
+
+class AddSpaceTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_should_add_space_cases(self):
+        test_cases = (
+            ('哈哈ABC', '哈哈 ABC'),
+            ('ABC中文', 'ABC 中文'),
+            ('哈哈##@@嘻嘻', '哈哈 ##@@ 嘻嘻'),
+            ('一2三4', '一 2 三 4'),
+            ('1二3四', '1 二 3 四'),
+            ('這是α', '這是 α'),
+            ('這是£', '這是 £'),
+            ('這是=', '這是 ='),
+        )
+
+        for data, answer in test_cases:
+            with self.subTest(data=data, answer=answer):
+                result = plugin.add_space(data)
+
+                self.assertEqual(result, answer)
+
+    def test_should_not_change_cases(self):
+        test_cases = (
+            'abcd α£ 1234',
+            '哈<some_tag/>啥',
+            '中文<p>啥</p>中文',
+            '中文 <p>啥</p> 中文',
+            '五&lt;六&gt;七',
+            '這&amp;還在',
+            'abc。123',
+            '123，abc',
+        )
+
+        for data in test_cases:
+            with self.subTest(data=data):
+                result = plugin.add_space(data)
+
+                self.assertEqual(result, data)
